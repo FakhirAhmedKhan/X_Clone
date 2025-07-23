54601ddb0ebea5c1f0829b9a8899f7109979c3ce
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-07-23 21:33:00 +0400

    fix

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
   base: process.env.ViteBase || "/X_Clone/",
 });
